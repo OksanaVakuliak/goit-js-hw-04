1bf2a6ff3f9cb9816474ae1d9cf88a5b9e5eb20c
Oksana Vakuliak <oksanabagrova19@gmail.com>
2025-10-27 19:23:20 +0100

    solved tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,19 @@
-function slugify(title) {
-  title = title.toLowerCase();
-  const words = title.split(" ");
-  const slug = words.join("-");
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  const productsValues = Object.values(products);
+  let sum = 0;
+  for (const value of productsValues) {
+    sum += value;
+  }
+  if (sum <= containerSize) {
+    return true;
+  }
+  return false;
 }
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
